705896e0cb8120becd469f5357f15a2da0545334
Skye Bleed <skyebleed@mailfence.com>
2019-10-01 10:59:51 -0500

    Start building syntax for the command module

--- src/command/main.rs
+++ src/command/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
+const HELP_STR: &str = "IMPERFECT...\n\nSyntax: i [command] [arg]\n\nCommands:\n  install\tinstall a package\n  remove\tremove a package\n  find\t\tsearch for a package\n  upgrade\tupgrade a package";
+
 fn main() {
-    println!("hello from the command module!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("{}", HELP_STR);
+        std::process::exit(1);
+    }
+
+    let cmd: &str = args[1].as_str();
+    let arg: &str = args[2].as_str();
+
+    match cmd {
+        "install" => return,
+        "remove" => return,
+        "find" => return,
+        "upgrade" => return,
+        _ => {
+            println!("Command {} does not exist...", cmd);
+            std::process::exit(1);
+        }
+    };
 }
