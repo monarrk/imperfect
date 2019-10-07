010862c58dc1c8d7476ff7363bf35c95a9ef69e6
Skye Bleed <skyebleed@mailfence.com>
2019-10-07 11:23:04 -0500

    Makes requests to the AUR

--- src/command/install.rs
+++ src/command/install.rs
@@ -1,4 +1,9 @@
-pub fn install(arg: &str) {
+use hyper::Client;
+use hyper::Uri;
+
+const AUR_URL: &str = "http://aur.archlinux.org/";
+
+pub async fn install(arg: &str) -> Result<(), hyper::error::Error> {
     let args: Vec<&str> = arg.split("::").collect();
     if args.len() < 2 {
         println!("Please enter a package name and repo like pkg::repo");
@@ -6,10 +11,32 @@ pub fn install(arg: &str) {
     }
 
     match args[1] {
-        "aur" => println!("AUR"),
+        "aur" => aur_download(args[0]).await,
         _ => {
             println!("Repo \"{}\" is not supported", args[1]);
             std::process::exit(1);
         }
-    };
+    }.expect("Failed to get pkg");
+
+    Ok(())
+}
+
+async fn aur_download(pkgname: &str) -> Result<(), hyper::error::Error> {
+    println!("Downloading {} from the AUR", pkgname);
+
+    let url = format!("{}{}.git", AUR_URL, pkgname).as_str().parse::<Uri>().unwrap();
+
+    println!("\tMaking request to {}", &url);
+    let client = Client::new();
+    let res = client.get(url).await?;
+    println!("\tResponse: {}", res.status());
+
+    let mut body = res.into_body();
+
+    while let Some(next) = body.next().await {
+        let chunk = next?;
+        println!("{:?}", &chunk);
+    }
+
+    Ok(())
 }
--- src/command/main.rs
+++ src/command/main.rs
@@ -1,10 +1,15 @@
+#![feature(termination_trait_lib)]
+#![deny(warnings)]
+#![warn(rust_2018_idioms)]
+
 use std::env;
 
 mod install;
 
 const HELP_STR: &str = "IMPERFECT...\n\nSyntax: i [command] [arg]\n\nCommands:\n  install\tinstall a package\n  remove\tremove a package\n  search\tsearch for a package\n  update\tupgrade a package\n  repo\t\t[sync|add|remove] syncs, adds, or removes a package repository\n  sync\t\tsyncs all repos and updates all packages";
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), hyper::error::Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         println!("{}", HELP_STR);
@@ -16,14 +21,16 @@ fn main() {
 
     match cmd {
         "install" => install::install(arg),
-        "remove" => return,
-        "search" => return,
-        "update" => return,
-        "repo" => return,
-        "sync" => return,
+        //"remove" => cmd,
+        //"search" => cmd,
+        //"update" => cmd,
+        //"repo" => cmd,
+        //"sync" => cmd,
         _ => {
             println!("Command {} does not exist...", cmd);
             std::process::exit(1);
         }
-    };
+    }.await?;
+
+    Ok(())
 }
